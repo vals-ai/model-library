46d3fbd8117742c29e35e4213ab23a60cdd8681d
Langston Nashold <lgnashold@gmail.com>
2025-12-10 23:03:51 -0800

    No more timeouts (#371)
    GitOrigin-RevId: 0ecc4f56f9ab0b6775342c3d75add0321dfcfedc

--- model_library/utils.py
+++ model_library/utils.py
@@ -39,7 +39,7 @@ def deep_model_dump(obj: object) -> object:
 
 def default_httpx_client():
     return httpx.AsyncClient(
-        timeout=httpx.Timeout(timeout=2 * 60 * 60, connect=5 * 60),
+        timeout=httpx.Timeout(None),
         limits=httpx.Limits(
             max_connections=2000, max_keepalive_connections=300
         ),  # TODO: increase, but make sure prod enough sockets to not hit file descriptor limit
